cec6d5c49e7bd7f47d5ee31c0e94d6347aeffd26
EinoRuuth <eino@ruuth.net>
2023-09-25 11:29:15 +0300

    tein hommia

--- Lentopeli.py
+++ Lentopeli.py
@@ -1,4 +1,35 @@
 import connector
+import random as rd
+
+#config starts
+sqlpassword="admin"
+itemamount = 2
+airportamount = 2
+#config ends
+
+color = ["green", "blue", "red", "yellow", "white"]
+item = ["apple", "stick", "chair", "ball", "clock"]
+items = []
+
+def hakija(limit):
+    sql = "SELECT name FROM airport"
+    sql += " ORDER BY RAND ( )"
+    sql += " LIMIT "+str(limit)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0 :
+        print(tulos)
+    return
 
 # laita allaolevaan sulkujen sisään oman salasanasi
-yhteys = connector.sqlyhteys("admin")
+yhteys = connector.sqlyhteys(sqlpassword)
+
+hakija(airportamount)
+for number in range(itemamount):
+    itemcolor = rd.randint(0,4)
+    itemname = rd.randint(0,4)
+    itemfullname = color[itemcolor]+" "+item[itemname]
+    items.append(itemfullname)
+
+print(items)
