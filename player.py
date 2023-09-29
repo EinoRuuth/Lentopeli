be2b70008cfc6b1261894d33a032db48e0f5bd17
onnimej <142915449+onnimej@users.noreply.github.com>
2023-09-29 13:06:54 +0300

    player muuttu

--- player.py
+++ player.py
@@ -1,7 +1,15 @@
 import connector
 
+yhteys = connector.sqlyhteys("admin")
+
 id= 1 
 fuel_budget = 1000
 screen_name = "player"
 fuel_left = 1000
 treasures = 0
+
+def delete():
+    sql = "delete from players"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    print(kursori.rowcount, "rows cleared.")
