e09a4506a958d854d049e36f673ae2f1b5483c45
onnimej <142915449+onnimej@users.noreply.github.com>
2023-09-29 14:18:42 +0300

    tein insert into players taululle

--- player.py
+++ player.py
@@ -12,19 +12,8 @@ treasures = 0
 def hae(items,players,yhteys):
     playerss = []
     for playername in players:
-        sql = "INSERT INTO players (screen_name) VALUES (%s)"
+        sql = "INSERT INTO players (id, fuel_budget, fuel_left, treasures, location, screen_name) VALUES (%s)"
         val = (playername)
         kursori = yhteys.cursor()
         kursori.execute(sql, val)
-    for itemname in items:
-        itemairport = rd.randint(0,len(players)-1)
-        while itemairport in playerss and itemairport<len(players):
-            itemairport=itemairport+1
-        playerss.append(itemairport)
-        itemairport = players[itemairport]
-        for x in itemairport:
-            itemairport=x
-        sql = "UPDATE game SET treasure='"+itemname+"' WHERE airport_name='"+itemairport+"'"
-        kursori = yhteys.cursor()
-        kursori.execute(sql)
     return
