1ee044c557d418126be236bbdc803de2b4b3c5a2
EinoRuuth <eino@ruuth.net>
2023-09-29 12:40:28 +0300

    nyt password annetaan command line argumenttina

--- Lentopeli.py
+++ Lentopeli.py
@@ -3,18 +3,20 @@ import gamecreator
 import random as rd
 import sys
 
+clargs = (sys.argv)
+clargs.pop(0)
+
+if len(clargs) > 0:
+    sqlpassword=clargs[0]
+
 #config starts
 #laita tähän alle oma sql salasanasi
-sqlpassword="admin"
 gamecountry = "FI"
 itemamount = 2
 airportamount = 20
 itemtons = ["8 tonnia ", "11.5 tonnia ", "13 tonnia ", "15 tonnia ", "16 tonnia "]
 itemnames = ["aurinkopaneeleita", "stick", "chair", "ball", "clock"]
 #config ends
-
-clargs = (sys.argv)
-clargs.pop(0)
 
 def delete():
     sql = "delete from game"
@@ -24,7 +26,7 @@ def delete():
 
 yhteys = connector.sqlyhteys(sqlpassword)
 itemsandairports = gamecreator.airports_items(itemamount, airportamount, itemtons, itemnames, gamecountry, yhteys)
-if len(clargs) > 0 and clargs[0] == "del":
+if len(clargs) > 1 and clargs[1] == "del":
     delete()
 else:
     gamecreator.sqlinsert(itemsandairports[0], itemsandairports[1], yhteys)
