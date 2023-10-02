e48ceb743ea647185e4da97075db5de0a0f2690d
LenniMarks <123360910+LenniMarks@users.noreply.github.com>
2023-10-02 10:02:36 +0300

    Lisäsin player gamecreator ja lisäsin playerin tiedostoon homebase haun

--- player.py
+++ player.py
@@ -3,8 +3,14 @@ import connector
 
 yhteys = connector.sqlyhteys("admin")
 
-def hae(players,yhteys):
-    takenplayers = []
+def hakija(airport_name, yhteys):
+    sql = "SELECT airport_name FROM game"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+
+def player_info(players,yhteys):
     for playername in players:
         sql = "INSERT INTO players (id, fuel_budget, fuel_left, treasures, location, screen_name) VALUES (%s, %s, %s, %s, %s, %s)"
         val = (playername[0], 0, 0)
