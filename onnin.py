e041119316a9d578673843371a2245aa478d022c
onnimej <142915449+onnimej@users.noreply.github.com>
2023-10-03 16:53:09 +0300

    Update onnin.py

--- onnin.py
+++ onnin.py
@@ -19,7 +19,17 @@ def hakija( yhteys):
 # tee move funktioon sille että se laittaa siihen location ton airport muuttujan
 # ja katsoo polttoaineen. se returnaa true jos on polttoainetta vielä mutta jos ei ole se returnaa false
 def move(airport, yhteys):
-    return
+    lentokoneen_polttoaine = yhteys['fuel_left']
+    nykyinen_sijainti = yhteys['location']
+    polttoaineen_kulutus_per_lento = yhteys['fuel_budget']
+    etaisyys = abs(nykyinen_sijainti - airport)
+    tarvittava_polttoaine = etaisyys * polttoaineen_kulutus_per_lento
+
+    if lentokoneen_polttoaine >= tarvittava_polttoaine:
+        yhteys['fuel_budget'] -= tarvittava_polttoaine
+        return True
+    else:
+        return False
 
 
 lentokenttä = hakija(yhteys)
