8048ac1a386835a5e90d638e3423bd7791e3ad1f
onnimej <142915449+onnimej@users.noreply.github.com>
2023-10-06 13:56:31 +0300

    move muutoksia

--- move.py
+++ move.py
@@ -1,46 +1,69 @@
 import connector
 
-yhteys = connector.sqlyhteys("admin")
+yhteys = connector.sqlyhteys("menat44")
 
 
 def hakija( yhteys):
-    sql = "SELECT name FROM game"
+    sql = "SELECT airport_name FROM game"
     # randomi order että ei ole aakkosjärjestys
     sql += " ORDER BY RAND ( )"
     # limitoi etsinnät 1
-    sql += " LIMIT "+1
+    sql += " LIMIT 1 "
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
-    return tulos
+    return tulos[0][0]
+
+def treasure_check(aarre, tavarat, yhteys):
+    lentokentan_nimi = tavarat[0][0]
+    aarteen_nimi = aarre[0][0]
+    merkkijono = ""
+    if aarre[0][0]:
+        sql1 = "UPDATE players SET treasures='"+aarre[0][0]+"' WHERE id='"+"1"+"'"
+        sql2 = "UPDATE game SET treasure='"+"(NULL)"+"' WHERE treasure='"+str(aarteen_nimi)+"'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql1)
+        kursori = yhteys.cursor()
+        kursori.execute(sql2)
+        merkkijono = (f"{lentokentan_nimi}ssä on aarre. Aarre lisätty ruumaan")
+    else:
+        merkkijono = (f"{lentokentan_nimi}ssä ei ole aarretta")
+    return merkkijono
 
 
-# tehtävä:
-# tee move funktioon sille että se laittaa siihen location ton airport muuttujan
-# ja katsoo polttoaineen. se returnaa true jos on polttoainetta vielä mutta jos ei ole se returnaa false
+def treasure_haku(tavarat, yhteys):
+    lentokentan_nimi = tavarat[0][0]
+    sql = "SELECT treasure FROM game"
+    sql += " WHERE (airport_name='"+lentokentan_nimi+"')"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    aarre = kursori.fetchall()
+    print(treasure_check(aarre, lentokenttä, yhteys))
+    return
+
+
 def move(airport, yhteys):
-    return
+    sql = "SELECT fuel_left FROM players"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    fuel= kursori.fetchall()
+    fuel = fuel[0][0]
+    if fuel >= 100:
+        fuelleft = fuel-100
+        sql1 = "UPDATE players SET location='" + airport + "'"
+        sql2 = "UPDATE players SET fuel_left='" + str(fuelleft) + "'"
+        sql3 = "UPDATE game SET has_visited=%s WHERE airport_name='"+ airport + "'"
+        val3 = (1,)
+        kursori.execute(sql1)
+        kursori.execute(sql2)
+        kursori.execute(sql3,val3)
+        treasure_haku(lentokenttä, yhteys)
+        return True
+    else:
+        return False
 
 
-# tehtävä2:
-# tehtävä on kattoo onko lentokentällä arretta, jos siel on niin se laittaa sen player tietokanna treasure kohtaan
-# ja poistaa sen sieltä game taulusta sekä returnaa on true
-# jos ei ole niin se returnaa false
-def treasure(airport, yhteys):
-    return
-
-
-polttoaineloppu = True
-while polttoaineloppu:
-    lentokenttä = hakija(yhteys)
-    liikkuminen = move(lentokenttä, yhteys)
-    if not liikkuminen:
-        polttoaineloppu = False
-    treasure(lentokenttä, yhteys)
-exit("GAMER OVER!\nYou ran out of fuel.")
-
-#move hommma tarvii:
-# lokaation päivitys player tietokantaan
-# polttoaineen chekki jos ei riitä niin peli loppuu ja päivitys player tietokantaan
-# treasure chekki ja sen lisäys player hommaa, ja poistaa game taulusta
-# homabase chekki jos on tavara niin polttoainetta ja jos on saanut kaikki tavarat niin voittaa (pitää laskua kuinka monta arretta on viety dificultia vastaan)
+lentokenttä = hakija(yhteys)
+liikkuminen = move(lentokenttä, yhteys)
+if not liikkuminen:
+    exit("GAMER OVER!\nYou ran out of fuel.")
