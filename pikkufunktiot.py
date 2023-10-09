5de81e7d44a9ab7054d2b6dc62584f35b6157c1a
EinoRuuth <eino@ruuth.net>
2023-10-09 10:20:59 +0300

    pikkufunktioiden tehtävänanto filessä

--- pikkufunktiot.py
+++ pikkufunktiot.py
@@ -16,3 +16,11 @@ def delete(kursori):
     sql = "delete from players"
     kursori.execute(sql)
     print(kursori.rowcount, "rows cleared.")
+
+# pikkufunktioita jotka tarvitsen vähintään keskiviikkona saa tehä jos haluaa lisää osaa projektiin
+
+# treasureamount funktio joka katsoo kuinka monta aarretta game taulussa lentokentissä vielä on
+
+# fuelamount joka ottaa tietokannasta kuinka paljon polttoainetta on jäljellä
+
+# pelaajanlokaatio eli funktio joka antaa pelaajan sen hetkisen lokaation players taulusta
