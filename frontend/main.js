543d7fdeb25575ac6f5b3bc82e9acaedb780d163
LenniMarks <123360910+LenniMarks@users.noreply.github.com>
2023-12-01 13:16:11 +0200

    Karttojoen sijaintien muutoksia ja onnin tehtävä kommentoitu

--- frontend/main.js
+++ frontend/main.js
@@ -6,12 +6,26 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 }).addTo(map);
 map.setView([60, 24], 7);
 
+// Onnin tehtävä
+// Tee tähän funktio joka ottaa Pelaajan tiedot backendistä lähetetystä jsonista
+// Laita tiedot kartan vieressä oleviin laatikoihin nätin näköisesti
 
 
 
 
 
+
+
+
+
+
+
+//
+
+
+// Lentokenttien iconit kartalla
 const blueIcon = L.divIcon({className: 'blue_icon'});
+const greenIcon = L.divIcon({className: 'green_icon'});
 
 
 // Pyytää lentokenttien kordinaatit
@@ -19,12 +33,38 @@ async function gameSetup(){
   const response = await fetch('http://127.0.0.1:3000/creategame/20/200/jason')
   .then((response) => response.json())
   .then((location) => {
+    let player = location[0].data.playerdata
     let airports = location[0].data.gamedata
-    console.log(airports[0])
+    console.log(player)
+    console.log(airports)
+
+    // Laittaa lentokenttien sijainnit kartalle
     for (let i = 0; i < airports.length; i++) {
       const marker = L.marker([airports[i].latitude, airports[i].longitude]).addTo(map);
-      marker.bindPopup(`You are here: <b>${airports[i].name}</b>`);
-      marker.setIcon(blueIcon);
+
+      // Kenttä missä pelaajan on
+      if (player.longitude === airports[i].longitude && player.latitude === airports[i].latitude) {
+        marker.bindPopup(`Olet täällä: <b>${airports[i].name}</b>`);
+        marker.openPopup();
+        marker.setIcon(greenIcon);
+      }
+
+      // Muut kentät
+      else {
+        marker.setIcon(blueIcon);
+        const popupContent = document.createElement('div');
+        const h4 = document.createElement('h4');
+        h4.innerHTML = airports[i].name;
+        popupContent.append(h4);
+        const goButton = document.createElement('button');
+        goButton.classList.add('Fly-Button');
+        goButton.innerHTML = 'Lennä tänne';
+        popupContent.append(goButton);
+        const p = document.createElement('p');
+        p.innerHTML = `Mahdollisuus: ${airports[i].treasurechance} %`;
+        popupContent.append(p);
+        marker.bindPopup(popupContent);
+      }
     }
   });
 }
